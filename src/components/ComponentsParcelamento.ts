b14816700920192bcd25fbd9040d3c1c549fb9a6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-10 20:15:02 -0300

    criando components customizados para pagina de parcelamento

---
+++ src/components/ComponentsParcelamento.ts
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+export const BoxParcela = styled(Box)`
+  padding: 20px;
+  border: 1px solid #E5E5E5;
+`;
+
+export const BoxPixTag = styled(Box)`
+  position: absolute;
+  top: -16px;
+  left: 16px;
+  font-weight: bold;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #E5E5E5;
+`;
