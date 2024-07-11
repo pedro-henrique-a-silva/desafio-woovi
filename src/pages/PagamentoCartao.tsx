ce9c550b89fa15acfe5ef7592e63ae45010b151e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-11 19:28:28 -0300

    criando pagina pagamento com cartão

--- src/pages/PagamentoCartao.tsx
+++ src/pages/PagamentoCartao.tsx
@@ -1,6 +1,43 @@
+import { Container, Grid } from "@mui/material"
+import Header from "../components/Header"
+import PrazoPagamento from "../components/PrazoPagamento"
+import ParcelasPagar from "../components/ParcelasPagar"
+import Footer from "../components/Footer"
+import FormCartao from "../components/FormCartao"
+import { useContext } from "react"
+import context from "../context/context"
+
 function PagamentoCartao() {
+  const {selectedValue} = useContext(context)
+
+  const defaultParcelas = selectedValue > 1 
+    ? selectedValue - 1 
+    : 1
   return (
-    <div>PagamentoCartao</div>
+    <>
+       <Header 
+          title={`João, pague o restante em ${defaultParcelas} x no cartão`}
+        />
+      <Container>
+      <Grid 
+          container 
+          rowSpacing={1} 
+          columnSpacing={{ xs: 0, sm: 2, md: 3 }}
+          columns={{ xs: 1, sm: 8, md: 12 }}
+        >
+          <Grid item xs={12} >
+            <FormCartao />
+          </Grid>
+
+          <Grid item xs={4}>
+            <PrazoPagamento />
+            <ParcelasPagar />
+          </Grid>
+
+        </Grid>
+      </Container>
+      <Footer/>
+    </>
   )
 }
 
