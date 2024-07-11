009fb4771434ccfc6cf765e11ecf646315cb5dc2
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-07-11 09:38:41 -0300

    add css base line

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom"
 import PagamentoPix from "./pages/PagamentoPix"
 import PagamentoCartao from "./pages/PagamentoCartao"
 import Parcelamento from "./pages/Parcelamento"
+import { CssBaseline } from "@mui/material"
 
 function App() {
 
   return (
     <>
+      <CssBaseline/>
       <Routes>
         <Route path="/" element={<Parcelamento/>}></Route>
         <Route path="/pagamento-pix" element={<PagamentoPix/>}></Route>
